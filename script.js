c30e36d0c099f2f75479aee1ed33b08ce70f9903
Luis Renan Pavanello <luisrpavanello@gmail.com>
2023-05-17 14:23:45 -0400

    Add modify to PR

--- script.js
+++ script.js
@@ -4,24 +4,29 @@ const wayFinderlines = document.getElementsByClassName('navigation_options')[0];
 const shadowedsurface = document.getElementsByClassName('shadowed_surface')[0];
 const closeMobileButton = document.getElementsByClassName('closemob-table')[0];
 const explorer = document.getElementsByClassName('explorer');
+
 buttonswitch.addEventListener('click', () => {
   wayFinderlines.classList.toggle('active');
   shadowedsurface.classList.toggle('active');
   buttonswitch.classList.toggle('active');
   closeMobileButton.classList.toggle('active');
 });
+
 closeMobileButton.addEventListener('click', () => {
   wayFinderlines.classList.toggle('active');
   shadowedsurface.classList.toggle('active');
   buttonswitch.classList.toggle('active');
   closeMobileButton.classList.toggle('active');
 });
+
 for (let i = 0; i < explorer.length; i += 1) {
   explorer[i].addEventListener('click', () => {
-    wayFinderlines.classList.toggle('active');
-    shadowedsurface.classList.toggle('active');
-    buttonswitch.classList.toggle('active');
-    closeMobileButton.classList.toggle('active');
+    if (window.innerWidth <= 768) { // Adjust the width value according to your responsive design breakpoints
+      wayFinderlines.classList.toggle('active');
+      shadowedsurface.classList.toggle('active');
+      buttonswitch.classList.toggle('active');
+      closeMobileButton.classList.toggle('active');
+    }
   });
 }
 // My Latest Projects
