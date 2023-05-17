0c52989d6ac1dbf939ebe65469eb85d1c4d98e5b
Luis Renan Pavanello <luisrpavanello@gmail.com>
2023-05-17 13:01:10 -0400

    Add content in file JS

--- script.js
+++ script.js
@@ -24,3 +24,100 @@ for (let i = 0; i < explorer.length; i += 1) {
     closeMobileButton.classList.toggle('active');
   });
 }
+// My Latest Projects
+const projectsList = [
+  {
+    name: 'Project 1',
+    description: 'Briefly introduce your project and its key features.',
+    technologies: ['software one', 'software two', 'software three', 'software four'],
+    MobImage: 'mobpattern.jpg',
+    DeskImage: 'psnap.jpg',
+    sharepoint: 'https://www.google.com/',
+    reference: 'https://www.github.com/',
+  },
+  {
+    name: 'Project 2',
+    description: 'Briefly introduce your project and its key features.',
+    technologies: ['software one', 'software two', 'software three', 'software four'],
+    MobImage: 'idplaceholder.png',
+    DeskImage: 'idplaceholder.png',
+    sharepoint: '#',
+    reference: '#',
+  },
+  {
+    name: 'Project 3',
+    description: 'Briefly introduce your project and its key features.',
+    technologies: ['software one', 'software two', 'software three', 'software four'],
+    MobImage: 'idplaceholder.png',
+    DeskImage: 'idplaceholder.png',
+    sharepoint: '#',
+    reference: '#',
+  },
+  {
+    name: 'Project 4',
+    description: 'Briefly introduce your project and its key features.',
+    technologies: ['software one', 'software two', 'software three', 'software four'],
+    MobImage: 'idplaceholder.png',
+    DeskImage: 'idplaceholder.png',
+    sharepoint: '#',
+    reference: '#',
+  },
+  {
+    name: 'Project 5',
+    description: 'Briefly introduce your project and its key features.',
+    technologies: ['software one', 'software two', 'software three', 'software four'],
+    MobImage: 'idplaceholder.png',
+    DeskImage: 'idplaceholder.png',
+    sharepoint: '#',
+    reference: '#',
+  },
+  {
+    name: 'Project 6',
+    description: 'Briefly introduce your project and its key features.',
+    technologies: ['software one', 'software two', 'software three', 'software four'],
+    MobImage: 'idplaceholder.png',
+    DeskImage: 'idplaceholder.png',
+    sharepoint: '#',
+    reference: '#',
+  },
+];
+// #Pattern of Cards
+const patternPopUp = document.getElementsByClassName('patternholder')[0];
+const coverupBg = document.getElementById('coverup');
+const shareP = document.getElementById('see-live');
+const reference = document.getElementById('see-source');
+let currentScrollPosition = window.scrollY;
+function Showpattern(index) {
+  const project = projectsList[index];
+  currentScrollPosition = window.scrollY;
+  patternPopUp.classList.toggle('active');
+  coverupBg.classList.toggle('active');
+  window.scrollTo(0, 0);
+  patternPopUp.querySelector('h3').textContent = project.name;
+  patternPopUp.querySelectorAll('.badge').forEach((badge, index) => {
+    badge.textContent = project.technologies[index];
+  });
+  patternPopUp.querySelector('p').textContent = project.description;
+  shareP.setAttribute('onclick', `window.location.href='${project.sharepoint}'`);
+  reference.setAttribute('onclick', `window.location.href='${project.reference}'`);
+}
+const projectiddesing = document.getElementById('iddesing');
+const projectsholder = document.querySelector('.projects');
+for (let i = 0; i < projectsList.length; i += 1) {
+  const projectid = projectiddesing.content.cloneNode(true);
+  const project = projectsList[i];
+  projectid.querySelector('h3').textContent = project.name;
+  projectid.querySelectorAll('.badge').forEach((badge, index) => {
+    badge.textContent = project.technologies[index];
+  });
+  projectid.querySelector('.button').textContent = 'See Project';
+  projectid.querySelector('.button').addEventListener('click', () => {
+    Showpattern(i);
+  });
+  projectsholder.appendChild(projectid);
+}
+function Camouflagepattern() { // eslint-disable-line no-unused-vars
+  patternPopUp.classList.toggle('active');
+  coverupBg.classList.toggle('active');
+  window.scrollTo(0, currentScrollPosition);
+}
